c7c0f4623c9c11115bb46c5c6d225861c4dc741c
ah-designs <ahdesigns@outlook.com>
2016-01-25 20:00:51 +0000

    finished styling the buttons, fix a bug with my form, change some of the font families and created them variables, repositioned my main nav so it response better to different size browsers, change font sizes into ems, add animsition jquery plug in.

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+
+    $('.animsition').animsition({
+
+    inClass: 'zoom-in',
+    outClass: 'zoom-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link'
+
+
+
+    });
